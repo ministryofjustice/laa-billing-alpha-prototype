e108219ec67eea7d00ab385477ed8678adda374f
Jonathan Porton <jonathan@mubudigital.co.uk>
2024-03-28 10:53:03 +0000

    Data updates and filter work

--- app/routes.js
+++ app/routes.js
@@ -8,5 +8,41 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
+router.get('/my-claims', function(request, res) {
+  
+    var claimNo = request.session.data.claimNo;
+    var theClaim = null;
+  
+    // find the application
+    for (const clm of request.session.data.sampleCaseListData) {
+        if (clm.claimRef === request.session.data.claimNo)
+        theClaim = clm;
+      }
+});
 
 
+// URL test submit
+router.get('/url-routing', function(request, response) {
+
+  var theClaim = null;
+
+  var claimNo = request.session.data.claimNo;
+
+  for (const clm of request.session.data.sampleCaseListData) {
+    if (clm.claimRef === request.session.data.claimNo)
+    theClaim = clm;
+  }
+
+  response.render("playground/result")
+
+})
+
+// URL test submit
+router.get('/url-simple', function(request, response) {
+
+  var claimNo = request.session.data.claimNo;
+
+    theClaim = claimNo;
+    response.render("playground/result") + "?" + theClaim
+
+})
